0349ce4b542d84257b3b4c6b284cd826e90b9b95
410902054 <71751283+410902054@users.noreply.github.com>
2020-11-18 12:42:55 +0800

    nothing important

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 basic.forever(function () {
     for (let i = 0; i <= 4; i++) {
         for (let j = 0; j <= i; j++) {
-            led.plot(0, 4 - (i + 1))
+            led.plot(0, 0 + i)
         }
-        basic.pause(500)
+        basic.pause(200)
     }
     basic.clearScreen()
 })
